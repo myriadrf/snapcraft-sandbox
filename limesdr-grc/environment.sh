d6471b7bad1f6acc8e6103fabb3006c17b563d38
Josh Blum <josh@joshknows.com>
2016-10-06 15:14:19 -0700

    use VOLK_PREFIX for grc, gqrx

--- limesdr-grc/environment.sh
+++ limesdr-grc/environment.sh
@@ -4,5 +4,6 @@ export SOAPY_SDR_ROOT=$SNAP
 export APPDATA=$SNAP_USER_COMMON
 export GRC_BLOCKS_PATH=$SNAP\share\gnuradio\grc\blocks
 export VOLK_CONFIGPATH=$SNAP_USER_COMMON
+export VOLK_PREFIX=$SNAP
 
 $@
