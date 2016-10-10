47708c9eb5a4918a2d5a471e0d008077461a0165
Josh Blum <josh@joshknows.com>
2016-10-09 18:17:06 -0700

    gnuradio snap branch, extra deps added

--- limesdr-grc/environment.sh
+++ limesdr-grc/environment.sh
@@ -2,8 +2,10 @@
 
 export SOAPY_SDR_ROOT=$SNAP
 export APPDATA=$SNAP_USER_COMMON
-export GRC_BLOCKS_PATH=$SNAP\share\gnuradio\grc\blocks
+export GRC_BLOCKS_PATH=$SNAP/share/gnuradio/grc/blocks
 export VOLK_CONFIGPATH=$SNAP_USER_COMMON
 export VOLK_PREFIX=$SNAP
+export GR_PREFIX=$SNAP
+export PYTHONPATH=$SNAP/lib/python2.7/dist-packages/
 
 $@
